9b7b41665a96b5797c66c64e101e71e0d9e81c85
Nick Jacobs <nickjacobs@Nicks-MacBook-Pro-2.local>
2020-04-16 10:04:52 +1200

    Add testpage for dashboard

---
+++ app/src/pagetypes/TestPage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace {
+
+
+    class IndicatorPage extends Page
+    {
+
+        private static $db = [ 
+
+        ];
+
+        private static $has_one = [
+           
+        ];
+
+        
+
+        public function getCMSFields()
+        {
+            $fields = parent::getCMSFields();
+
+          
+            return $fields;
+        }
+
+    }
+
+}
