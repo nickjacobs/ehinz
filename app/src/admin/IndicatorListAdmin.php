da5f783a3c8d5dbf112e8d10ebdbae50794f3cc0
Nick Jacobs <hello@nickjacobs.net.nz>
2023-04-03 13:24:16 +1200

    Increase pagination length on modeladmin for indicator list

--- app/src/admin/IndicatorListAdmin.php
+++ app/src/admin/IndicatorListAdmin.php
@@ -1,15 +1,32 @@
 <?php
 
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridFieldPaginator;
 
-class IndicatorListAdmin extends ModelAdmin 
+class IndicatorListAdmin extends ModelAdmin
 {
 
     private static $managed_models = [
-        'IndicatorListItem'        
+        'IndicatorListItem'
     ];
 
     private static $url_segment = 'indicator-list';
 
     private static $menu_title = 'Indicator List';
+
+
+    public function getEditForm($id = null, $fields = null) {
+        $form = parent::getEditForm($id, $fields);
+        $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
+        if ($gridField) {
+            $config = $gridField->getConfig();
+            $paginator = $config->getComponentByType('GridFieldPaginator');
+            if ($paginator) {
+                $paginator->setItemsPerPage(1000);
+            } else {
+                $config->addComponent(new GridFieldPaginator(1000));
+            }
+        }
+        return $form;
+    }
 }
