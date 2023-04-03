026d09dc41e21f3ec4cfdd8a2dc46f3b768ea6c2
Nick Jacobs <hello@nickjacobs.net.nz>
2023-04-03 13:26:53 +1200

    remove

--- app/src/admin/IndicatorListAdmin.php
+++ app/src/admin/IndicatorListAdmin.php
@@ -15,18 +15,5 @@ class IndicatorListAdmin extends ModelAdmin
     private static $menu_title = 'Indicator List';
 
 
-    public function getEditForm($id = null, $fields = null) {
-        $form = parent::getEditForm($id, $fields);
-        $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
-        if ($gridField) {
-            $config = $gridField->getConfig();
-            $paginator = $config->getComponentByType('GridFieldPaginator');
-            if ($paginator) {
-                $paginator->setItemsPerPage(1000);
-            } else {
-                $config->addComponent(new GridFieldPaginator(1000));
-            }
-        }
-        return $form;
-    }
+    
 }
